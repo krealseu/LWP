ddca596752d02e67fac353e68dad87f03b67e584
CC <ltee12@hotmail.com>
2017-12-23 10:39:01 +0800

    由AS2.3 到 AS3,修改gradle

--- app/src/main/java/org/kreal/lwp/MainActivity.kt
+++ app/src/main/java/org/kreal/lwp/MainActivity.kt
@@ -41,10 +41,10 @@ class MainActivity : AppCompatActivity(), android.app.LoaderManager.LoaderCallba
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
