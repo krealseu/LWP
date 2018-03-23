bcac74d74c61cf95c9f609bb9f2518ab87fadd47
CC <ltee12@hotmail.com>
2018-03-23 19:00:22 +0800

    更换imageAdapter

--- app/src/main/java/org/kreal/lwp/share/ShareDealActivity.kt
+++ app/src/main/java/org/kreal/lwp/share/ShareDealActivity.kt
@@ -9,9 +9,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.Gravity
-import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.Button
@@ -19,6 +17,9 @@ import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import org.kreal.lwp.R
+import org.kreal.lwp.adapters.ImageAdapter
+import org.kreal.lwp.adapters.ImageAdapterData
 
 class ShareDealActivity : AppCompatActivity() {
 
@@ -44,7 +45,7 @@ class ShareDealActivity : AppCompatActivity() {
         val button = Button(baseContext)
         val buttonLayout = FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         buttonLayout.gravity = Gravity.BOTTOM or Gravity.CENTER
-        button.text = "Save"
+        button.setText(R.string.save)
         button.background.alpha = 160
         button.setOnClickListener {
             ShareDealService.startAction(baseContext, arrayListOf(uri))
@@ -68,11 +69,11 @@ class ShareDealActivity : AppCompatActivity() {
         val point = Point()
         display.getSize(point)
         val num = if (point.x >= 1000) point.x / 500 else 2
-        val adapter = ImageAdapter(arrayList, point.y / num)
+        val adapter = ImageAdapter(ImageAdapterData(arrayList), (point.y / num))
         recyclerView.layoutManager = GridLayoutManager(baseContext, num)
         recyclerView.adapter = adapter
 
-        button.text = "Save"
+        button.setText(R.string.save)
         button.background.alpha = 160
         button.setOnClickListener {
             ShareDealService.startAction(baseContext, arrayList)
@@ -82,37 +83,10 @@ class ShareDealActivity : AppCompatActivity() {
 
     private fun showError() {
         val textView = TextView(baseContext)
-        textView.text = "Error Share"
+        textView.setText(R.string.error_share)
         textView.setTextColor(Color.RED)
         textView.textSize = 30f
         textView.gravity = Gravity.CENTER
         setContentView(textView)
     }
-
-    class ImageAdapter(private val data: ArrayList<Uri>, private val imageHeight: Int) : RecyclerView.Adapter<ImageAdapter.ItemHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ItemHolder {
-            val imageView = ImageView(parent?.context)
-            imageView.minimumHeight = imageHeight
-            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-            val layoutParams = RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT)
-            val margin = 2
-            layoutParams.setMargins(margin, margin, margin, margin)
-            imageView.layoutParams = layoutParams
-            return ItemHolder(imageView)
-        }
-
-        override fun getItemCount(): Int = data.size
-
-        override fun onBindViewHolder(holder: ItemHolder?, position: Int) {
-            holder?.apply {
-                Glide.with(imageView).load(data[position]).into(imageView)
-            }
-        }
-
-        class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            val imageView: ImageView = itemView as ImageView
-        }
-    }
-
 }
